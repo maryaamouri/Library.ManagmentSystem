774fd11f30fe94d64ac05eb4185f3711930a7a79
Marya_Amouri <marya.ammori@gmail.com>
2023-09-08 11:37:09 +0300

    registration application extension method

---
+++ Core/Libro.Application/Extensions/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using FluentValidation;
+using Libro.Application.Authors;
+
+namespace Libro.Application.Extensions
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetCallingAssembly();
+
+            //services.Scan(b => b.FromAssemblies(assembly)
+            //    .AddClasses(c => c.AssignableTo(typeof(IService<,>)))
+            //    .AsImplementedInterfaces()
+            //    .WithSingletonLifetime());
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddScoped<IAuthorService, AuthorService>();
+
+            services.AddAutoMapper(typeof(AuthorProfile));
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
