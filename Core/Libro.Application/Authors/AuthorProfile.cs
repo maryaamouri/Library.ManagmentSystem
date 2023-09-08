8641655ffffa29fe0c1d5cee151ec02de19e21f7
Marya_Amouri <marya.ammori@gmail.com>
2023-09-08 11:35:37 +0300

    add author profile

---
+++ Core/Libro.Application/Authors/AuthorProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Libro.Domain.Authors;
+
+namespace Libro.Application.Authors
+{
+    public class AuthorProfile : Profile
+    {
+        public AuthorProfile()
+        {
+            CreateMap<AuthorRequest, Author>();
+            CreateMap<Author, AuthorDto>();
+        }
+    }
+}
