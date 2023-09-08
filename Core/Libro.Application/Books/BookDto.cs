36c511a655d7170327e3d57e35a24d1534adc1d6
Marya_Amouri <marya.ammori@gmail.com>
2023-09-08 11:51:11 +0300

    add book Dto

---
+++ Core/Libro.Application/Books/BookDto.cs
@@ -0,0 +1,16 @@
+﻿using Libro.Domain.Authors;
+
+
+namespace Libro.Application.Books
+{
+    public class BookDto
+    {
+        public Guid BookId { get; private set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; } = string.Empty;
+        public string? Genre { set; get; } = string.Empty;
+        public Guid? AuthorId { get; set; }
+        public Author? Author { get; set; } = null;
+        public DateTime? PublicationDate { get; set; } = null;
+    }
+}
