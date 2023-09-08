09142a5323333f4e2f26c4c9497a56258b9e7080
Marya_Amouri <marya.ammori@gmail.com>
2023-09-08 11:36:35 +0300

    add author custom exception

---
+++ Core/Libro.Application/Authors/Exceptions/AuthorAreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿using Libro.Application.Common.Exceptions;
+
+namespace Libro.Application.Authors.Exceptions
+{
+    internal class AuthorAreadyExistsException : BadRequestException
+    {
+        public AuthorAreadyExistsException(string name)
+            : base($"Author With Name {name} Already Exits")
+        {
+        }
+    }
+}
