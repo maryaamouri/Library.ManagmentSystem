fcdef49f591ef2a73edcc0cbe0d2336425e8b41a
Marya_Amouri <marya.ammori@gmail.com>
2023-09-08 11:51:38 +0300

    add book custom exceptions

---
+++ Core/Libro.Application/Books/Exceptions/BookAlreadyExists.cs
@@ -0,0 +1,10 @@
+﻿namespace Libro.Application.Books.Exceptions
+{
+    internal class BookAlreadyExists : BadRequestException
+    {
+        public BookAlreadyExists(string title)
+            : base($"Book With Title {title} Already Exits")
+        {
+        }
+    }
+}
