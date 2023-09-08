59d0f92ed7bfa4e17f96987b68fb46c36c41991a
Marya_Amouri <marya.ammori@gmail.com>
2023-09-08 12:32:54 +0300

    add transaction resource to domain

---
+++ Core/Libro.Domain/Transactions/ITransactionRepository.cs
@@ -0,0 +1,8 @@
+﻿using Libro.Domain.Common;
+
+namespace Libro.Domain.Transactions
+{
+    public interface ITransactionRepository : IRepository<Transaction>
+    {
+    }
+}
---
+++ Core/Libro.Domain/Transactions/Transaction.cs
@@ -0,0 +1,20 @@
+﻿namespace Libro.Domain.Transactions
+{
+    public class Transaction
+    {
+        public Guid TransactionId { set; get; }
+        public Guid BookId { get; set; }
+        public Guid PatronId { get; set; }
+        public Guid? LibrarianId { get; set; }
+        public DateTime BorrowedDate { set; get; }
+        public DateTime? DueDate { set; get; }
+        public Status Status { get; set; }
+        public DateTime? ActualReturnedDate { set; get; }
+    }
+    public enum Status
+    {
+        Accepted,
+        Rejected,
+        Pinned
+    }
+}
