d5c1a6929dd9752875e03259db229bb77be8d475
Marya_Amouri <marya.ammori@gmail.com>
2023-09-09 15:23:23 +0300

    configure database

---
+++ Infrastructure/Libro.Persistence/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Libro.Persistence.DbModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Libro.Persistence.Configurations
+{
+    internal sealed class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transactions");
+
+            builder.HasKey(transaction => transaction.TransactionId);
+
+            builder.Property(transaction => transaction.BorrowedDate)
+              .HasColumnType("datetime");
+
+            builder.Property(transaction => transaction.DueDate)
+                .HasColumnType("datetime");
+
+            builder.Property(transaction => transaction.ActualReturnedDate)
+                .HasColumnType("datetime");
+
+            builder.HasOne<Book>()
+                .WithMany()
+                .HasForeignKey(t => t.BookId);
+
+            builder.HasOne<UserProfile>()
+                .WithMany()
+                .HasForeignKey(u => u.LibrarianId);
+
+            builder.HasOne<UserProfile>()
+                .WithMany()
+                .HasForeignKey(u => u.PatronId);
+        }
+    }
+}
---
+++ Infrastructure/Libro.Persistence/Configurations/UserProfileConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Libro.Persistence.DbModels;
+using Libro.Persistence.Seeders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace Libro.Persistence.Configurations
+{
+    internal sealed class UserProfileConfiguration : IEntityTypeConfiguration<UserProfile>
+    {
+        public void Configure(EntityTypeBuilder<UserProfile> builder)
+        {
+            builder.ToTable("UserProfiles");
+            builder.HasKey(user => user.UserProfileId);
+ 
+            ProfileSeeder.Seed(builder);
+        }
+    }
+}
