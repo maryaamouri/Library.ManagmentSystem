7e4ad7bdb07c5b703f966b149ed9d0f29cbd654f
Marya_Amouri <marya.ammori@gmail.com>
2023-09-09 15:27:01 +0300

    Add Entities for Identity

---
+++ Infrastructure/Libro.Identity/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Libro.Identity.Entities
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
---
+++ Infrastructure/Libro.Identity/Entities/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Libro.Identity.Entities
+{
+    public class UserProfile 
+    {
+        public Guid UserProfileId { get; set; }
+        public Guid UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
