df5e57be91e4fcb34f9d258191b08bc2195bb637
Marya_Amouri <marya.ammori@gmail.com>
2023-09-09 15:24:40 +0300

    Registration of persistence

---
+++ Infrastructure/Libro.Persistence/Extensions/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using Libro.Domain.Authors;
+using Libro.Domain.Books;
+using Libro.Persistence.Profiles;
+using Libro.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Libro.Persistence.Extensions
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddPersistence
+        (
+            this IServiceCollection services,
+            IConfiguration configuration
+        )
+        {
+            var assembly = Assembly.GetCallingAssembly();
+
+            services.AddDbContext<LiboDbContext>
+                (options => options
+                .UseSqlServer(configuration.GetConnectionString("DatabaseConnectionString"))
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
+
+            //services.Scan(b => b.FromAssemblies(assembly)
+            //     .AddClasses(c => c.AssignableTo(typeof(IRepository<>)))
+            //     .AsImplementedInterfaces()
+            //     .WithSingletonLifetime());
+
+            services.AddScoped<IAuthorRepository, AuthorRepository>();
+            services.AddScoped<IBookRepository, BookRepository>();
+
+            services.AddAutoMapper(typeof(AuthorProfile));
+            services.AddAutoMapper(typeof(BookProfile));
+            services.AddAutoMapper(typeof(UserProfile));
+            services.AddAutoMapper(typeof(TransactionProfile));
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
