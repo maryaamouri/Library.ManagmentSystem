8ec1e4fd7b680cb7aa1bd08cd62be063977b17a5
Marya_Amouri <marya.ammori@gmail.com>
2023-09-09 15:26:46 +0300

    add DbModels Configuration

---
+++ Infrastructure/Libro.Identity/Configurations/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Libro.Identity.Entities;
+using Libro.Identity.Seeders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Libro.Identity.Configurations
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            ApplicationUser[] users = UserSeeder.Seed();
+            builder.HasData(users);
+        }
+    }
+}
---
+++ Infrastructure/Libro.Identity/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Libro.Identity.Seeders;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Libro.Identity.Configurations
+{
+    internal class UserRoleConfiguration : IEntityTypeConfiguration<IdentityUserRole<Guid>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserRole<Guid>> builder)
+        {
+            builder.HasData
+            (
+                UserRoleSeeder.Seed()
+            );
+        }
+    }
+}
