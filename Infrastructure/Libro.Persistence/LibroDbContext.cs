d9fca091a53e5b17924e1414c660af72bcb54d75
Marya_Amouri <marya.ammori@gmail.com>
2023-09-09 15:23:44 +0300

    add application dbContext

---
+++ Infrastructure/Libro.Persistence/LibroDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Libro.Persistence.DbModels;
+using Libro.Persistence.Configurations;
+
+namespace Libro.Persistence
+{
+    public sealed class LiboDbContext : DbContext
+    {
+        public LiboDbContext(DbContextOptions<LiboDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<UserProfile> Users { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("Library");
+            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+            modelBuilder.ApplyConfiguration(new UserProfileConfiguration());
+            modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
