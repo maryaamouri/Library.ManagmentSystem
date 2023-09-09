97f3b9a274cecb42e195aaa5d88942c18db87a26
Marya_Amouri <marya.ammori@gmail.com>
2023-09-09 15:27:24 +0300

    Registeration of Identity

---
+++ Infrastructure/Libro.Identity/Extensions/Extensions.cs
@@ -0,0 +1,58 @@
+﻿using Libro.Application.Identity;
+using Libro.Application.Identity.Models;
+using Libro.Identity.Entities;
+using Libro.Identity.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Libro.Identity.Extensions
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddIdentityInfrastructure
+        (this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+            var jwtSettings = configuration.GetSection("JwtSettings").Get<JwtSettings>();
+            services.AddDbContext<LibroIdentityDbContext>
+                (options => options
+                .UseSqlServer(configuration.GetConnectionString("DatabaseConnectionString"))
+                .EnableSensitiveDataLogging()
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
+
+            services.AddIdentity<ApplicationUser, IdentityRole<Guid>>()
+                    .AddEntityFrameworkStores<LibroIdentityDbContext>()
+                    .AddDefaultTokenProviders();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(o =>
+                {
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero,
+                        ValidIssuer = configuration["JwtSettings:Issuer"],
+                        ValidAudience = configuration["JwtSettings:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]))
+                    };
+                });
+            
+
+            services.AddScoped<IAuthService, AuthService>();
+            return services;
+        }
+    }
+}
