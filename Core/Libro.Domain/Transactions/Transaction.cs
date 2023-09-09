ce78d9b46a155c4a051a02b00bfa30d91780edd2
Marya_Amouri <marya.ammori@gmail.com>
2023-09-09 15:30:38 +0300

    Edit Transaction Entity

--- Core/Libro.Domain/Transactions/Transaction.cs
+++ Core/Libro.Domain/Transactions/Transaction.cs
@@ -8,13 +8,8 @@
         public Guid? LibrarianId { get; set; }
         public DateTime BorrowedDate { set; get; }
         public DateTime? DueDate { set; get; }
-        public Status Status { get; set; }
+        public bool? IsAcepted { get; set; }
+        public bool IsCanceled { get; set; } = false;   
         public DateTime? ActualReturnedDate { set; get; }
-    }
-    public enum Status
-    {
-        Accepted,
-        Rejected,
-        Pinned
     }
 }
