0e4b7964ba37c5ced5b869febf7cabfd27043f51
Marya_Amouri <marya.ammori@gmail.com>
2023-09-09 15:30:04 +0300

    Rename Profile to UserProfile

---
+++ Core/Libro.Domain/UserProfiles/IProfileRepository.cs
@@ -0,0 +1,9 @@
+﻿using Libro.Domain.Common;
+
+
+namespace Libro.Domain.UserProfiles
+{
+    public interface IUserProfileRepository : IRepository<UserProfile>
+    {
+    }
+}
---
+++ Core/Libro.Domain/UserProfiles/UserProfile.cs
@@ -0,0 +1,22 @@
+﻿using Libro.Domain.Books;
+using Libro.Domain.Transactions;
+
+namespace Libro.Domain.UserProfiles
+{
+    public class UserProfile
+    {
+        public Guid UserProfileId { get; set; }
+        public IList<Transaction> Transactions { get; private set; } = new List<Transaction>();
+        public IList<Book> BorrowdBooks { get; private set; } = new List<Book>();
+        public void AddBorrowdBook(Book book)
+        {
+            if (book is not null)
+                BorrowdBooks.Add(book);
+        }
+        public void AddTransaction(Transaction tran)
+        {
+            if (tran is not null)
+                Transactions.Add(tran);
+        }
+    }
+}
