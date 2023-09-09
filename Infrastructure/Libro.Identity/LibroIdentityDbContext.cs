32b977ee854ea5a7bb1a68edfcec16c3890f4fe0
Marya_Amouri <marya.ammori@gmail.com>
2023-09-09 15:28:45 +0300

    add Auth service

---
+++ Infrastructure/Libro.Identity/LibroIdentityDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Libro.Identity.Configurations;
+using Libro.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Libro.Identity
+{
+    public class LibroIdentityDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+
+    {
+        public LibroIdentityDbContext(DbContextOptions<LibroIdentityDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new RoleConfiguration());
+            builder.ApplyConfiguration(new UserProfileConfiguration());
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new UserRoleConfiguration());
+        }
+    }
+}
---
+++ Infrastructure/Libro.Identity/Services/AuthService.cs
@@ -0,0 +1,130 @@
+﻿using Libro.Application.Identity;
+using Libro.Application.Identity.Models;
+using Libro.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Libro.Identity.Services
+{
+    internal class AuthService : IAuthService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly JwtSettings _jwtSettings;
+
+        public AuthService(UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager,
+            IOptions<JwtSettings> jwtSettings)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public async Task<LoginResponse> Login(LoginRequest request)
+        {
+            //Console.WriteLine($"_jwtSettings {_jwtSettings.Key} is now null");
+            var user = await _userManager.FindByEmailAsync(request.Email) 
+                ?? throw new Exception($"User {request.Email} was not found.");
+            var result = await _signInManager.PasswordSignInAsync(user.UserName, request.Password, false, lockoutOnFailure: false);
+
+            if (!result.Succeeded)
+            {
+                throw new Exception($"Credentials for '{request.Email} aren't valid'.");
+            }
+            
+            JwtSecurityToken jwtSecurityToken = await GenerateToken(user);
+
+            var response = new LoginResponse
+            {
+                Id = user.Id,
+                Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+                Email = user.Email,
+                UserName = user.UserName
+            };
+
+            return response;
+        }
+
+        private async Task<JwtSecurityToken> GenerateToken(ApplicationUser user)
+        {
+        
+            
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var roleClaims = new List<Claim>();
+
+            for (int i = 0; i < roles.Count; i++)
+            {
+                roleClaims.Add(new Claim(ClaimTypes.Role, roles[i]));
+            }
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("Uid", user.Id.ToString())
+            }
+            .Union(userClaims)
+            .Union(roleClaims);
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.DurationInMinutes),
+                signingCredentials: signingCredentials);
+            return jwtSecurityToken;
+        }
+
+        public async Task<RegistrationResponse> Register(RegisrationRequest request)
+        {
+            var existingUser = await _userManager.FindByNameAsync(request.UserName);
+
+            if (existingUser != null)
+            {
+                throw new Exception($"Username '{request.UserName}' already exists.");
+            }
+
+            var user = new ApplicationUser
+            {
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                UserName = request.UserName,
+                EmailConfirmed = true
+            };
+
+            var existingEmail = await _userManager.FindByEmailAsync(request.Email);
+
+            if (existingEmail == null)
+            {
+                var result = await _userManager.CreateAsync(user, request.Password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, "Employee");
+                    return new RegistrationResponse() { UserId = user.Id.ToString() };
+                }
+                else
+                {
+                    throw new Exception($"{result.Errors}");
+                }
+            }
+            else
+            {
+                throw new Exception($"Email {request.Email} already exists.");
+            }
+        }
+    }
+}
