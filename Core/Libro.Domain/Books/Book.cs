1083d267f78c8356973ef3f8bd21bb5680459214
Marya_Amouri <marya.ammori@gmail.com>
2023-09-23 20:38:12 +0300

    Edit Domain Entities add IsAvilable property to Book

--- Core/Libro.Domain/Books/Book.cs
+++ Core/Libro.Domain/Books/Book.cs
@@ -11,6 +11,7 @@ namespace Libro.Domain.Books
         public Guid? AuthorId { get; set; }
         public Author? Author { get; set; } = null;
         public DateTime? PublicationDate { get; set; } = null;
+        public bool IsAvailable { get; set; } = true;
 
     }
 }
