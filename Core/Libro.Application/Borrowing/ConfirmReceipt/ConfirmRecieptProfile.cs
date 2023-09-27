670f1009e9a99cadb66bf1c6c02b2b7eba636c00
Marya_Amouri <marya.ammori@gmail.com>
2023-09-27 21:52:18 +0300

    add confirm receipt feature

---
+++ Core/Libro.Application/Borrowing/ConfirmReceipt/ConfirmRecieptProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Libro.Application.Transations;
+
+namespace Libro.Application.Borrowing.ConfirmReceipt
+{
+    internal class ConfirmRecieptProfile : Profile
+    {
+        protected ConfirmRecieptProfile()
+        {
+            CreateMap<ConfirmReciptCommand,TransactionDto>();
+        }
+    }
+}
