2577a087596990f67cb61a023be5d6ddb0f870bc
Marya_Amouri <marya.ammori@gmail.com>
2023-10-04 14:37:47 +0300

    add validation behavior for Application.BurrowingService

---
+++ Core/Libro.Application/Common/PipelineBehaviors/ValidationBehavior.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Libro.Application.Common.PipelineBehaviors
+{
+    public class ValidationBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var validationFailures = await Task.WhenAll(
+                _validators.Select(v => v.ValidateAsync(request, cancellationToken))
+                );
+            var failures = validationFailures
+                .SelectMany(result => result.Errors)
+                .Where(failure => failure != null)
+                .ToList();
+
+            if (failures.Any())
+            {
+                throw new FluentValidation.ValidationException(failures);
+            }
+
+            return await next();
+
+        }
+    }
+}
