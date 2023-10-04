14b67bd8884f115cc459021cdeae101f286053b0
Marya_Amouri <marya.ammori@gmail.com>
2023-10-04 14:35:49 +0300

    update domain.UserProfileRepository contract

---
+++ Core/Libro.Domain/UserProfiles/IUserProfileRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Libro.Domain.UserProfiles
+{
+    public interface IUserProfileRepository 
+    {
+        Task<UserProfile> CreateAsync(UserProfile userProfile);
+        Task UpdateAsync(UserProfile userProfile);
+        Task<UserProfile> GetByIdAsync(Guid id);
+    }
+}
