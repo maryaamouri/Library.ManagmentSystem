eecf37b8b74b51f2ff6e0b815e1da4018e953113
Marya_Amouri <marya.ammori@gmail.com>
2023-10-04 14:33:23 +0300

    update domain.book

--- Core/Libro.Domain/Books/Book.cs
+++ Core/Libro.Domain/Books/Book.cs
@@ -4,14 +4,23 @@ namespace Libro.Domain.Books
 {
     public class Book
     {
+        public Book(Guid bookId, string title, string? description, string? genre, Guid? authorId, DateTime? publicationDate)
+        {
+            BookId = bookId;
+            Title = title;
+            Description = description;
+            Genre = genre;
+            AuthorId = authorId;
+            PublicationDate = publicationDate;
+        }
+
         public Guid BookId { get; private set; }
         public required string Title { get; set; }
         public string? Description { get; set; } = string.Empty;
         public string? Genre { set; get; } = string.Empty;
-        public Guid? AuthorId { get; set; }
-        public Author? Author { get; set; } = null;
+        public Guid? AuthorId { get; set; } = null;
+        public Author? Author { get;  set; } = null;
         public DateTime? PublicationDate { get; set; } = null;
-        public bool IsAvailable { get; set; } = true;
-
+        public bool IsAvailable { get; internal set; } = true;
     }
 }
