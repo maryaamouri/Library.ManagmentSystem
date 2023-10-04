30ac9493dbc3bc98c435ca3c6e0c9f5c79a8fe36
Marya_Amouri <marya.ammori@gmail.com>
2023-10-04 14:47:30 +0300

    Setup Infrastructure Authomapper

---
+++ Infrastructure/Libro.Identity/Profiles/RoleProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Libro.Application.Identity.Services.UserInfo;
+using Microsoft.AspNetCore.Identity;
+
+namespace Libro.Identity.Profiles
+{
+    internal class RoleProfile : Profile
+    {
+        public RoleProfile()
+        {
+            CreateMap<IdentityRole<Guid>, Role>()
+              .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Name))
+              .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => src.Id))
+              .ReverseMap();
+        }
+    }
+}
---
+++ Infrastructure/Libro.Identity/Profiles/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Libro.Application.Identity.Services.UserInfo;
+using Libro.Identity.Entities;
+
+namespace Libro.Identity.Profiles
+{
+    internal class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<ApplicationUser, User>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.Id))
+                .ReverseMap();
+        }
+    }
+}
