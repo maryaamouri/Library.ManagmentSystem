344239fdc8f51e2d366bcb8f012a3ed684db03d0
Marya_Amouri <marya.ammori@gmail.com>
2023-10-04 14:43:06 +0300

    add login validation

--- Core/Libro.Application/Identity/Services/Login/LoginValidation.cs
+++ Core/Libro.Application/Identity/Services/Login/LoginValidation.cs
@@ -11,12 +11,12 @@ namespace Libro.Application.Identity.Services.Login
                 .NotEmpty().WithMessage("Email is required.")
                 .EmailAddress().WithMessage("Invalid email format.");
 
-            RuleFor(request => request.Password)
-                .NotEmpty().WithMessage("Password is required.")
-                .MinimumLength(6).WithMessage("Password must be at least 6 characters.")
-                .Matches(@"^(?=.*[a-z])").WithMessage("Password must contain at least one lowercase letter (a-z).")
-                .Matches(@"^(?=.*[A-Z])").WithMessage("Password must contain at least one uppercase letter (A-Z).")
-                .Matches(@"^(?=.*\d)").WithMessage("Password must contain at least one digit (0-9).");
+            //RuleFor(request => request.Password)
+            //    .NotEmpty().WithMessage("Password is required.")
+            //    .MinimumLength(6).WithMessage("Password must be at least 6 characters.")
+            //    .Matches(@"^(?=.*[a-z])").WithMessage("Password must contain at least one lowercase letter (a-z).")
+            //    .Matches(@"^(?=.*[A-Z])").WithMessage("Password must contain at least one uppercase letter (A-Z).")
+            //    .Matches(@"^(?=.*\d)").WithMessage("Password must contain at least one digit (0-9).");
         }
     }
 }
